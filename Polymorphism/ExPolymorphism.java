3c15ee8184a470156593e19bcb871698dc360fb0
prasad <raju.vydani@gmail.com>
2021-07-21 19:52:56 +0530

    polymorphism example

--- Polymorphism/ExPolymorphism.java
+++ Polymorphism/ExPolymorphism.java
@@ -4,32 +4,13 @@ class ExPolymorphism
 	{
 		System.out.println("YaY !!! Welcome to Polymorphism !!!");
 
-		Actor actor = null;
 
-		if(args[0].equalsIgnoreCase("Chiru")) {
-			
-			actor = new Chiru();
-			
-		} else if (args[0].equalsIgnoreCase("Pawan"))
-		{
-			actor = new PawanKalyan();
-
-		} else if (args[0].equalsIgnoreCase("Mahesh"))
-		{
-			actor = new Mahesh();
-		}
-
-		printDetails(actor);
 
 	}
 
 	public static void printDetails(Actor actor) {
 		
-		System.out.println("===============Profile===============>>");
-		actor.getProfile();
-
-		System.out.println("===============Movies list ===============");
-		actor.movies();
+		
 
 	}
 }
